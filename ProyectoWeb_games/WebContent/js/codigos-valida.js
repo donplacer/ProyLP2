143d62a7663852c2197aeeab01c0e41f4ec9c8cb
aries <aries@DESKTOP-DMLT94J>
2019-06-04 04:23:35 -0500

    s

---
+++ ProyectoWeb_games/WebContent/js/codigos-valida.js
@@ -0,0 +1,20 @@
+
+var imgcambio=["promocion2.png", "preventa2.png","mando2.png","superman2.png"];
+
+var imgorig=["promocion.png", "preventa.png",  "mando.png",  "superman.png"];
+
+function cambio(x){
+	document.getElementsByClassName("imgnav")[x].src="img/imagenesNavegacion/"+imgcambio[x];
+	document.querySelectorAll('h2')[x].style.color ="#00ff00";
+	
+}
+
+function original(x){
+	document.getElementsByClassName("imgnav")[x].src="img/imagenesNavegacion/"+imgorig[x];
+	document.querySelectorAll('h2')[x].style.color ="#5E5E5E";
+}
+
+
+
+
+
